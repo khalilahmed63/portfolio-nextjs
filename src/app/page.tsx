e5b89188f7ee0f0e74c4c769eb1acded9787cbcd
khalilahmed63 <panhwarkhalilahmed@gmail.com>
2023-06-10 00:29:42 +0500

    working with Skill section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
-import { FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import { FaInstagram, FaLinkedinIn, FaReact, FaTwitter } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 import { BsGithub } from "react-icons/bs";
 import Image from "next/image";
@@ -8,7 +8,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#272829]">
-      <div className="max-w-screen-2xl min-h-screen px-5 md:px-10 mx-auto flex items-center">
+      <div className="max-w-screen-2xl min-h-screen px-5 md:px-32 mx-auto flex items-center">
         <div className="mt-10 flex items-center justify-center">
           <Image
             src={"/assets/images/khalil-ahmed-1.jpg"}
@@ -28,7 +28,7 @@ export default function Home() {
               building excellent software that improve the lives of those around
               me.
             </p>
-            <button className="bg-[#FFB100] border-4 border-[#9A7318]/90 shadow-xl shadow-black hover:shadow-md hover:shadow-black mt-5 py-1 px-3 rounded-full duration-300 hover:scale-95">
+            <button className="bg-[#FFB100] border-4 border-[#9A7318] shadow-xl shadow-black hover:shadow-md hover:shadow-black mt-5 py-1 px-3 rounded-full duration-300 hover:scale-95">
               Download CV
             </button>
             <div className=" mt-16 flex">
@@ -89,7 +89,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="max-w-screen-2xl min-h-screen px-5 md:px-10 mx-auto flex items-cente">
+      <div className="max-w-screen-2xl min-h-screen px-5 md:px-32  mx-auto flex items-cente">
         <div className="mt-10 w-full ">
           <div className=" text-2xl items-center justify-center text-center">
             <h1 className="text-gray-500 text-7xl font-bold tracking-widest">
@@ -99,76 +99,59 @@ export default function Home() {
               ABOUT <span className="text-[#ffb400]">ME</span>
             </h2>
           </div>
-          <div className="mt-10 flex">
-            <div className="w-full border-2 border-red-500">
-              {" "}
-              <div className="col-md-6 col-sm-12 row">
-                <h3 className="text-white font-bold mb-3">PERSONAL INFO</h3>
-                <div className="col-6">
-                  <ul>
-                    <li className="font-bold">
-                      <p>
-                        {" "}
-                        First Name : <span className=" text-muted">Khalil</span>
-                      </p>
-                    </li>
-                    <li className="font-bold">
-                      <p>
-                        {" "}
-                        Age : <span className=" text-muted">20</span>
-                      </p>
-                    </li>
-                    <li className="font-bold">
-                      <p>
-                        {" "}
-                        Freelance :{" "}
-                        <span className=" text-muted">Avaliable</span>
-                      </p>
-                    </li>
-                    <li className="font-bold">
-                      <p>
-                        {" "}
-                        Phone :{" "}
-                        <span className=" text-muted">+923070599766</span>
-                      </p>
-                    </li>
-                  </ul>
+          <div className="my-20 flex">
+            <div className="w-full">
+              <div className="">
+                <h3 className="text-3xl text-white font-bold mb-3">
+                  PERSONAL INFO
+                </h3>
+                <div className="mt-10">
+                  <div className="flex mb-10">
+                    <p className="text-white font-bold w-[50%]">
+                      First Name :<span className=" text-gray-400">Khalil</span>
+                    </p>
+
+                    <p className="text-white font-bold w-[50%]">
+                      Age : <span className=" text-gray-400">20</span>
+                    </p>
+                  </div>
+                  <div className="flex mb-10">
+                    <p className="text-white font-bold w-[50%]">
+                      Freelance :
+                      <span className=" text-gray-400">Avaliable</span>
+                    </p>
+
+                    <p className="text-white font-bold w-[50%]">
+                      Phone :
+                      <span className=" text-gray-400">+923070599766</span>
+                    </p>
+                  </div>
                 </div>
-                <div className="col-6">
-                  <ul>
-                    <li className="font-bold">
-                      <p>
-                        {" "}
-                        Last Name : <span className=" text-muted">Ahmed</span>
-                      </p>
-                    </li>
-                    <li className="font-bold">
-                      <p>
-                        {" "}
-                        Natoinality :{" "}
-                        <span className=" text-muted">pakistan</span>
-                      </p>
-                    </li>
-                    <li className="font-bold">
-                      <p>
-                        {" "}
-                        Address : <span className=" text-muted">hyderabad</span>
-                      </p>
-                    </li>
-                    <li className="font-bold">
-                      <p>
-                        {" "}
-                        Language :{" "}
-                        <span className=" text-muted">
-                          English, Urdu, Sindhi
-                        </span>
-                      </p>
-                    </li>
-                  </ul>
+                <div className="">
+                  <div className="flex mb-10">
+                    <p className="text-white font-bold w-[50%]">
+                      Last Name :<span className=" text-gray-400">Ahmed</span>
+                    </p>
+                    <p className="text-white font-bold w-[50%]">
+                      Natoinality :
+                      <span className=" text-gray-400">pakistan</span>
+                    </p>
+                  </div>
+                  <div className="flex mb-10">
+                    <p className="text-white font-bold w-[50%]">
+                      Address :<span className=" text-gray-400">hyderabad</span>
+                    </p>
+                    <p className="text-white font-bold w-[50%]">
+                      Language :
+                      <span className=" text-gray-400">
+                        English, Urdu, Sindhi
+                      </span>
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="w-full border-2 border-green-500">
+            <div className="w-full">
               <div className=" ">
                 <div className="flex">
                   <div className="w-full rounded p-5 m-3 border-2 border-gray-400">
@@ -204,6 +187,110 @@ export default function Home() {
                     <p className="pt-2  ml-[20px] text-white">AWARDS WON</p>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-screen-2xl min-h-screen px-5 md:px-32  mx-auto flex items-cente">
+        <div className=" w-full ">
+          <div className=" text-2xl items-center justify-center text-center">
+            <h2 className="-mt-14 font-bold text-3xl text-white tracking-widest">
+              MY <span className="text-[#ffb400]">SkILLS</span>
+            </h2>
+          </div>
+          <div className="my-10">
+            <div className="flex flex-wrap justify-evenly w-full">
+              <div className="mt-4">
+                <div className="group border-8 border-[#ffb200] hover:border-[#9A7318] rounded-full p-10 hover:bg-[#ffb200] duration-300">
+                  <FaReact
+                    size={60}
+                    className="text-[#61DBFB] group-hover:text-white"
+                  />
+                </div>
+                <h1 className="mt-2 font-bold text-3xl text-center text-white">
+                  React
+                </h1>
+              </div>
+              <div className="mt-4">
+                <div className="group border-8 border-[#ffb200] hover:border-[#9A7318] rounded-full p-10 hover:bg-[#ffb200] duration-300">
+                  <FaReact
+                    size={60}
+                    className="text-[#61DBFB] group-hover:text-white"
+                  />
+                </div>
+                <h1 className="mt-2 font-bold text-3xl text-center text-white">
+                  React
+                </h1>
+              </div>
+              <div className="mt-4">
+                <div className="group border-8 border-[#ffb200] hover:border-[#9A7318] rounded-full p-10 hover:bg-[#ffb200] duration-300">
+                  <FaReact
+                    size={60}
+                    className="text-[#61DBFB] group-hover:text-white"
+                  />
+                </div>
+                <h1 className="mt-2 font-bold text-3xl text-center text-white">
+                  React
+                </h1>
+              </div>
+              <div className="mt-4">
+                <div className="group border-8 border-[#ffb200] hover:border-[#9A7318] rounded-full p-10 hover:bg-[#ffb200] duration-300">
+                  <FaReact
+                    size={60}
+                    className="text-[#61DBFB] group-hover:text-white"
+                  />
+                </div>
+                <h1 className="mt-2 font-bold text-3xl text-center text-white">
+                  React
+                </h1>
+              </div>
+            </div>
+
+            <div className="mt-6 flex flex-wrap justify-evenly w-full">
+              <div className="mt-4">
+                <div className="group border-8 border-[#ffb200] hover:border-[#9A7318] rounded-full p-10 hover:bg-[#ffb200] duration-300">
+                  <FaReact
+                    size={60}
+                    className="text-[#61DBFB] group-hover:text-white"
+                  />
+                </div>
+                <h1 className="mt-2 font-bold text-3xl text-center text-white">
+                  React
+                </h1>
+              </div>
+              <div className="mt-4">
+                <div className="group border-8 border-[#ffb200] hover:border-[#9A7318] rounded-full p-10 hover:bg-[#ffb200] duration-300">
+                  <FaReact
+                    size={60}
+                    className="text-[#61DBFB] group-hover:text-white"
+                  />
+                </div>
+                <h1 className="mt-2 font-bold text-3xl text-center text-white">
+                  React
+                </h1>
+              </div>
+              <div className="mt-4">
+                <div className="group border-8 border-[#ffb200] hover:border-[#9A7318] rounded-full p-10 hover:bg-[#ffb200] duration-300">
+                  <FaReact
+                    size={60}
+                    className="text-[#61DBFB] group-hover:text-white"
+                  />
+                </div>
+                <h1 className="mt-2 font-bold text-3xl text-center text-white">
+                  React
+                </h1>
+              </div>
+              <div className="mt-4">
+                <div className="group border-8 border-[#ffb200] hover:border-[#9A7318] rounded-full p-10 hover:bg-[#ffb200] duration-300">
+                  <FaReact
+                    size={60}
+                    className="text-[#61DBFB] group-hover:text-white"
+                  />
+                </div>
+                <h1 className="mt-2 font-bold text-3xl text-center text-white">
+                  React
+                </h1>
               </div>
             </div>
           </div>
