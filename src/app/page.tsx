971d5c568de6384bd8abac71312cc8e07569cd38
khalilahmed63 <panhwarkhalilahmed@gmail.com>
2023-06-12 17:00:31 +0500

    working with education & experience section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -309,7 +309,78 @@ export default function Home() {
               EXPERIENCE & <span className="text-[#ffb400]">EDUCATION</span>
             </h2>
           </div>
-          <div className="my-10">asdf</div>
+          <div className="my-10">
+            <div className="md:flex">
+              <div className="w-[50%]">
+                <p className="pb-6 font-bold text-2xl text-white">.Education</p>
+                <div className="mt-16 pr-2 pb-2 border-b">
+                  <p className=" font-bold text-base text-white">
+                    BACHELOR OF COMPUTER SCIENCE
+                  </p>
+                  <p className=" text-base text-gray-500">2018 - 2021</p>
+                  <p className="my-6 text-base text-white">
+                    UNIVERSITY OF SINDH JAMSHORO
+                  </p>
+                </div>
+                <div className="mt-16 pr-2 pb-2 border-b">
+                  <p className=" font-bold text-base text-white">
+                    Oracle(ERP) 6 month traning
+                  </p>
+                  <p className=" text-base text-gray-500">
+                    sep-2020 - Mar-2020
+                  </p>
+                  <p className="my-6 text-base text-white">
+                    (IMCS) Institute of Mathematics & Computer Science
+                  </p>
+                </div>
+                <div className="mt-16 pr-2 pb-2 border-b">
+                  <p className=" font-bold text-base text-white">
+                    (php) Web Development 4 Month Traning
+                  </p>
+                  <p className=" text-base text-gray-500">
+                    Feb-2022 - May-2022
+                  </p>
+                  <p className="my-6 text-base text-white">
+                    (HIST) Hidaya Institute of Science & technology
+                  </p>
+                </div>
+              </div>
+              <div className="pl-5 w-[50%]">
+                <p className="pb-6 font-bold text-2xl text-white">
+                  .Professional Experience
+                </p>
+                <div className="mt-16 pr-2 pb-2 border-b">
+                  <p className=" font-bold text-base text-white">
+                    (HIST) Hidaya Institute of Science & technology
+                  </p>
+                  <p className=" text-base text-gray-500">
+                    Feb-2022 - May-2022
+                  </p>
+                  <p className="my-6 text-base text-white">
+                    Full-Stack Developer
+                  </p>
+                </div>
+                <div className="mt-16 pr-2 pb-2 border-b">
+                  <p className=" font-bold text-base text-white">Digiventic</p>
+                  <p className=" text-base text-gray-500">
+                    Aug-2020 - jan-2021
+                  </p>
+                  <p className="my-6 text-base text-white">
+                    Front-End Developer
+                  </p>
+                </div>
+                <div className="mt-16 pr-2 pb-2 border-b">
+                  <p className=" font-bold text-base text-white">
+                    BACHELOR OF COMPUTER SCIENCE
+                  </p>
+                  <p className=" text-base text-gray-500">2018 - 2021</p>
+                  <p className="my-6 text-base text-white">
+                    UNIVERSITY OF SINDH JAMSHORO
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
